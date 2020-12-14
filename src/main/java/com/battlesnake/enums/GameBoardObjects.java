e9930cdb3f95c0bab7889b94097f24b267186551
anton300 <anton.zenin2@gmail.com>
2020-12-14 21:51:01 +0000

    Added our head in enum.

--- src/main/java/com/battlesnake/enums/GameBoardObjects.java
+++ src/main/java/com/battlesnake/enums/GameBoardObjects.java
@@ -4,7 +4,7 @@ package com.battlesnake.enums;
  * @author Anton Zenin
  */
 public enum GameBoardObjects {
-  EMPTY(0), SNAKE_HEAD(1), OBSTACLE(2), FOOD(3);
+  EMPTY(0), SNAKE_HEAD(1), OBSTACLE(2), FOOD(3), OUR_HEAD(4);
 
   public final int value;
 
